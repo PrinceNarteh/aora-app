3435bb42b07a0a74441f4900f50cc9abf763e021
Prince Narteh <prinarteh@gmail.com>
2024-04-20 20:17:53 +0000

    feat: add type aliases

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 
 const App = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-primary">
+    <View className="flex-1 justify-center items-center bg-primary ">
       <Text className="text-3xl text-white">Aora</Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Profile

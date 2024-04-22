e23572f9b8d1293f802b47bb871ca639b775891c
Prince Narteh <prinarteh@gmail.com>
2024-04-22 03:04:06 +0000

    feat: add onboarding page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,42 @@
-import { Text, View } from "react-native";
+import { images } from "@/constants";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-primary ">
-      <Text className="text-3xl text-white font-poppins-black">Aora</Text>
-      <Link href="/profile" style={{ color: "blue" }}>
-        Profile
-      </Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full justify-center items-center p-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="absolute w-[136px] h-[15px] -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-poppins-regular mt-7 text-center text-gray-100">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 

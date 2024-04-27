3d866e7beb7de96e8f25083212ce555bb278967c
Prince Narteh <prinarteh@gmail.com>
2024-04-26 18:00:42 -0700

    feat: list header component

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,11 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useGlobalContext } from "context/GlobalProvider";
+import { StatusBar } from "expo-status-bar";
+import { images } from "@/constants";
 
 const Home = () => {
+  const { user } = useGlobalContext();
+  console.log({ user });
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ $id: "1" }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <Text>{item.$id}</Text>}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-center flex-row mb-6">
+              <View>
+                <Text className="font-poppins-medium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-poppins-semibold text-white">
+                  {user.username}
+                </Text>
+              </View>
+              <View>
+                <Image
+                  source={images.logoSmall}
+                  resizeMode="contain"
+                  className="w-9 h-10"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      />
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 };
 
